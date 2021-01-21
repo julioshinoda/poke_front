2044d55fb47de48add76f86a70557c762c43f4c4
shinoda <julio@verifymyage.co.uk>
2021-01-20 23:27:23 -0300

    add port to start

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,140 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useEffect, useState } from 'react'
 
-export default function Home() {
+const Home = () => {
+  const [input, setInput] = useState('')
+  const [list, setList] = useState([]);
+  const subscribe = async (e) => {
+    e.preventDefault() // prevents page reload
+    try {
+      const res = await fetch('./api/subscribe', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          emailAddress: input
+        })
+      })
+
+      if (res.status === 200) {
+        alert('You are subscribed!')
+      } else {
+        alert('Sorry, something went wrong.')
+      }
+    } catch (err) {
+      alert('Sorry, something went wrong.')
+    }
+    // alert("You have subscribed!")
+    // alert(input)
+  }
+
+  const getList = async () => {
+    try {
+      const res = await fetch('http://localhost:9001/trade', {
+        method: 'get',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+
+      if (res.status === 200) {
+        const listRes = await res.json()
+        setList(listRes)
+        console.log(listRes)
+      } else {
+        alert('Sorry, something went wrong.')
+      }
+    } catch (err) {
+      console.log(err)
+      alert('Sorry, something went wrong.s')
+    }
+  };
+
+  useEffect(() => {
+    getList()
+  }, []);
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <div className='p-8 justify-center items-center flex flex-col'>
+        <form className='flex'>
+          <input className='bg-gray-200 shadow-inner rounded-l p-2 flex-1'
+            id='email'
+            type='email'
+            aria-label='email address'
+            placeholder='Enter your email address'
+            value={input}
+            onChange={e => setInput(e.target.value)} />
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+          <input className='bg-gray-200 shadow-inner rounded-l p-2 flex-1'
+            id='email'
+            type='email'
+            aria-label='email address'
+            placeholder='Enter your email address'
+            value={input}
+            onChange={e => setInput(e.target.value)} />
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <button className='bg-blue-600 hover:bg-blue-700 duration-300 text-white shadow p-2 rounded-r flex flex-col' type='submit' onClick={subscribe}>
+            Sign Up
+        </button>
+        </form>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+      </div>
+      <div className='p-1 justify-center items-center flex flex-col'>
+        <h2>List</h2>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+        <table class="shadow-lg bg-white">
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+          <tr>
+            <th>ID</th>
+            <th>Trainer One</th>
+            <th>Country</th>
+            <th>Fair trade</th>
+          </tr>
+          {list.map(item => {
+            return (
+              <tr>
+                <th class=" border text-left px-8 py-4">{item.id}</th>
+                <th class=" border text-left px-8 py-4 ">
+                  {item.first_trainer_list.map(p => {
+                    return (
+                      <div>
+                        {'id:' + p.id}
+                        <br></br>
+                        {'name:' + p.name}
+                        <br></br>
+                        {'base_experience:' + p.base_experience}
+                      </div>
+                    )
+                  })
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+                  }
+
+                </th>
+                <th class="border text-left px-8 py-4">  {item.second_trainer_list.map(p => {
+                    return (
+                      <div>
+                        {'id:' + p.id}
+                        <br></br>
+                        {'name:' + p.name}
+                        <br></br>
+                        {'base_experience:' + p.base_experience}
+                      </div>
+                    )
+                  })
+
+                  }</th>
+                <th class="bg-blue-100 border text-left px-8 py-4">{JSON.stringify(item.fair)}</th>
+              </tr>
+            );
+          })}
+
+        </table>
+      </div>
+
     </div>
   )
 }
+
+export default Home
